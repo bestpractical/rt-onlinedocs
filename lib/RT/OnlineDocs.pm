d30c2315961b88207b7c6507d3c569b154ee2a99
Kevin Falcone <falcone@bestpractical.com>
2011-10-27 11:22:13 -0400

    Shove the README into lib and generate it

--- lib/RT/OnlineDocs.pm
+++ lib/RT/OnlineDocs.pm
@@ -1,5 +1,55 @@
-# Copyright 1996-2008 Best Practical Solutions, LLC
-#
+=head1 NAME
+
+RT-OnlineDocs - Provides a browseable interface to the developer documentation
+
+=head1 DESCRIPTION
+
+RT::OnlineDocs adds a "Developer Documentation" menu to the RT user interface.
+This tool provides a browsable user interface to the RT API documentation for
+the running RT instance. I'm indebted to Audrey Tang who contributed the
+first version of this code.
+
+=head1 INSTALLATION
+
+=over
+
+=item perl Makefile.PL
+
+=item make
+
+=item make install
+
+May need root permissions
+
+=item Edit your /opt/rt4/etc/RT_SiteConfig.pm
+
+Add this line:
+
+    Set(@Plugins, qw(RT::OnlineDocs));
+
+or add C<RT::OnlineDocs> to your existing C<@Plugins> line.
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 AUTHOR
+
+Jesse Vincent - <jesse@bestpractical.com>
+Audrey Tang - <audreyt@audreyt.org>
+
+=head1 LICENCE AND COPYRIGHT
+
+This software is copyright (c) 1996-2011 by Best Practical Solutions.
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of version 2 of the GNU General Public License.
+
+=cut
 
 use strict;
 use warnings;
