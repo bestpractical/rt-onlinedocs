074714499c01501a77a6e458367a88008b6a7655
Alex Vandiver <alexmv@bestpractical.com>
2011-10-26 21:16:39 -0400

    Refactor common lib-finding out

--- lib/RT/OnlineDocs.pm
+++ lib/RT/OnlineDocs.pm
@@ -14,4 +14,23 @@ require Pod::Simple::HTML;
 
 our $VERSION = "0.10";
 
+sub lib_paths {
+    my $dirname   = "$RT::BasePath/lib";
+    my $localdir  =  $RT::LocalLibPath;
+    my $plugindir =  $RT::LocalPluginPath;
+
+    # We intentionally don't use the plugins API, as this also gets us
+    # plugins that are not currently enabled
+    my @plugins = ();
+    if(opendir(PLUGINS, $plugindir)) {
+        while(defined(my $plugin = readdir(PLUGINS))) {
+            next if($plugin =~ /^\./);
+            push(@plugins, "$plugindir/$plugin/lib");
+        }
+        closedir(PLUGINS);
+    }
+
+    return ($dirname, $localdir, @plugins);
+}
+
 1;
